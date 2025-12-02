63683e022e34d1e3756dd09e13fe2d2b40a7652e
1711Saykots <sinade431@gmail.com>
2025-12-02 21:03:15 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# kic
+# Aplikasi Tabungan Siswa Berbasis Web PHP Native
+
+Aplikasi Web Sederhana untuk Tabungan Siswa Di Sekolah
+Menggunakan PHP Native
+
+Selamat Mencoba, Semoga Berhasil
---
+++ footer.php
@@ -0,0 +1,8 @@
+	<footer class="main-footer">
+      <div class="pull-right hidden-xs">
+        <b>Version</b> BETA
+      </div>
+      <strong>Copyright &copy; Bayu Tutor 2020 
+      	<!--<a href="https://sistempintar.com">Sistem Pintar</a>.-->
+      </strong> All rights reserved.
+    </footer>
